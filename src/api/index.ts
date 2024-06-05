fece701000e8865b3ddfd71642e61f37ec1ff35c
Sahib.Aujla <sahibpreet568@gmail.com>
2024-06-05 17:04:01 -0400

    added prettier

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 // src/api.js
-import { User } from "../models";
+import { User } from '../models';
 
 // fragments microservice API to use, defaults to localhost:8080 if not set in env
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8081";
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
@@ -11,7 +11,7 @@ const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8081";
  */
 
 export async function getUserFragments(user: User) {
-  console.log("Requesting user fragments data...");
+  console.log('Requesting user fragments data...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments/?expand=0`, {
       // Generate headers with the proper Authorization bearer token to pass.
@@ -23,10 +23,10 @@ export async function getUserFragments(user: User) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log("Successfully got user fragments data", { data });
+    console.log('Successfully got user fragments data', { data });
     return data;
   } catch (err) {
-    console.error("Unable to call GET /v1/fragment", { err });
+    console.error('Unable to call GET /v1/fragment', { err });
   }
 }
 
@@ -42,20 +42,20 @@ export async function getOneFragment(user: User, id: string) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log("Successfully got user fragment data", { data });
+    console.log('Successfully got user fragment data', { data });
     return data;
   } catch (err) {
-    console.error("Unable to call GET /v1/fragment", { err });
+    console.error('Unable to call GET /v1/fragment', { err });
   }
 }
 
 export async function postUserFragment(user: User, text: string) {
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
-      method: "POST",
+      method: 'POST',
       headers: {
         ...user.authorizationHeaders(),
-        "Content-Type": "text/plain",
+        'Content-Type': 'text/plain',
       },
       body: text,
     });
@@ -64,12 +64,12 @@ export async function postUserFragment(user: User, text: string) {
       // accessing the entries
       console.log(pair);
     }
-    const l = res.headers.get("Location");
+    const l = res.headers.get('Location');
     console.log({ l });
 
     const resp = await res.json();
     return resp;
   } catch (error) {
-    console.error("Unable to call Post v1/fragments", { error });
+    console.error('Unable to call Post v1/fragments', { error });
   }
 }
